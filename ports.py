65f25fe7c569da2ab2c8d0315c5038d18b0e2c0c
Abe Diaz <aberoddia@gmail.com>
2023-10-19 10:56:21 -0400

    final

---
+++ ports.py
@@ -0,0 +1,14 @@
+#!/bin/env python3
+
+import re
+
+regexMatch = "[pP]orts?[ :\"]{1,3}([1-9]\d{1,4}|6[1-5]\d)"
+matcher = re.compile(regexMatch)
+
+with open("file", "r") as file:
+	lines = file.readlines()
+	for line in lines:
+		try:
+			print(matcher.findall(line)[0])
+		except IndexError:
+			continue
